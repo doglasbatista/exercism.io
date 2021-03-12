90ce5e2c324ecdfc99c2f9e2117307455dd6686c
doglasbatista <doglasbatista@gmail.com>
2021-03-12 20:09:53 +0000

    resolve until space age

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,6 +1,18 @@
+const RNA_MAP: {[props:string]:string} = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U"
+} as const;
+
 class Transcriptor {
 
-  toRna() {
+  toRna(dna:string) {
+    return dna.split("").map(d => { 
+        const rna = RNA_MAP[d]
+        if (rna === undefined) throw "Invalid input DNA."
+        return rna
+    }).join("")
   }
 }
 
--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,4 +1,46 @@
 class SpaceAge {
+    seconds:number
+    static earthRotation = 31557600
+
+    constructor(seconds:number) {
+      this.seconds = seconds
+    }
+
+    onEarth(){
+      return this.calcYears(1)
+    }
+
+    onMercury(){
+      return this.calcYears(0.2408467)
+    }
+
+    onVenus(){
+      return this.calcYears(0.61519726)
+    }
+
+    onMars(){
+      return this.calcYears(1.8808158)
+    }
+
+    onJupiter(){
+      return this.calcYears(11.862615)
+    }
+
+    onSaturn(){
+      return this.calcYears(29.447498)
+    }
+
+    onUranus(){
+      return this.calcYears(84.016846)
+    }
+
+    onNeptune(){
+      return this.calcYears(164.79132)
+    }
+
+    private calcYears(rotation:number) {
+      return Number((this.seconds / (SpaceAge.earthRotation * rotation)).toFixed(2))
+    }
 }
 
 export default SpaceAge;
